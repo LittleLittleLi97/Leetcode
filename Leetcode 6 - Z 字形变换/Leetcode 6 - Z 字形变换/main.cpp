bdbf38ac30f70de664c26d8d67d364938d088ad8
97 <595594189@qq.com>
2022-03-11 22:03:53 +0800

    update

--- Leetcode 6 - Z 字形变换/Leetcode 6 - Z 字形变换/main.cpp
+++ Leetcode 6 - Z 字形变换/Leetcode 6 - Z 字形变换/main.cpp
@@ -13,7 +13,19 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
+        if (numRows == 1) return s;
+        string row[numRows];
+        int location = 0;
+        int orient = 1;
+        for (int i = 0; i < s.length(); i++) {
+            row[location] += s[i];
+            if (location == 0) orient = 1;
+            else if (location == numRows - 1) orient = -1;
+            location += orient;
+        }
+        string out;
+        for (int i = 0; i < numRows; i++) out += row[i];
+        return out;
     }
 };
 
